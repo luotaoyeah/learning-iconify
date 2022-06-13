4a5beb465eb717fb9066676b7df7d51c09074fcf
luotao <luotaoyeah@gmail.com>
2022-06-13 21:20:41 +0800

    feat(vue): https://docs.iconify.design/icon-components/vue/

--- projects/vue/src/App.vue
+++ projects/vue/src/App.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>HELLO</div>
+    <fieldset>
+        <legend></legend>
+
+        <Icon flip="vertical" icon="mdi:heart" style="color: #ff0000; font-size: 48px"></Icon>
+        <Icon icon="mdi:heart" rotate="270" style="color: #ff0000; font-size: 48px"></Icon>
+        <Icon icon="mdi:heart" rotate="270" style="color: #ff0000" width="48"></Icon>
+        <Icon height="48" icon="mdi:heart" rotate="270" style="color: #ff0000"></Icon>
+        <Icon color="#ff0000" height="48" icon="mdi:heart" rotate="270"></Icon>
+    </fieldset>
 </template>
 
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+    import { Icon } from '@iconify/vue';
+</script>
 
 <style scoped></style>
